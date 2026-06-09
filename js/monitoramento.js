c30cde0419a05623778e658ec813e7ec4809bc7d
eduardo-gotardi9 <du.gotardi@gmail.com>
2026-06-09 04:02:52 -0300

    feat: adiciona barra de pesquisa; adiciona favicon;  melhora css geral

--- js/monitoramento.js
+++ js/monitoramento.js
@@ -2,12 +2,29 @@
 
 const mapa = document.querySelector('.mapa img');
 const container = document.querySelector('.container');
+const containerPrevisao = document.querySelector('.container-previsao');
 const periodo = document.getElementById('periodo');
 const conteudo = document.getElementById('conteudo-historico');
+const botaoPesquisarEndereco = document.querySelector('.botao-pesquisar-endereco');
+const botaoPesquisarGeral = document.querySelector('.botao-pesquisar-geral');
 
-mapa.addEventListener('click', function() {
-    container.style.display = 'flex';
-});
+function mostrarCards() {
+    if (container.classList.contains('active')) {
+        container.classList.remove('active');
+    }
+    container.classList.add('active');
+
+    if (containerPrevisao) {
+        if (containerPrevisao.classList.contains('active')) {
+            containerPrevisao.classList.remove('active');
+        }
+        containerPrevisao.classList.add('active');
+    }
+}
+
+mapa.addEventListener('click', mostrarCards);
+botaoPesquisarEndereco.addEventListener('click', mostrarCards);
+botaoPesquisarGeral.addEventListener('click', mostrarCards);
 
 periodo.addEventListener('change', function() {
     const valor = this.value;
@@ -15,8 +32,10 @@ periodo.addEventListener('change', function() {
     if (valor === 'ultimo-ano') {
         conteudo.innerHTML = '<p>Últimos 12 meses: O total anual foi de 1.480 mm.</p>';
     } else if (valor === '6-meses') {
-        conteudo.innerHTML = '<p>mês 1:Foram registrados 142 mm de chuva no mês<br>mês 2:Foram registrados 138 mm de chuva no mês<br>mês 3:Foram registrados 100 mm de chuva no mês<br>mês 4:Foram registrados 80 mm de chuva no mês</p><p>mês 5:Foram registrados 180 mm de chuva no mês<br><p>mês 6:Foram registrados 157 mm de chuva no mês<br> ';
+        conteudo.innerHTML = '<p>Mês 1: Foram registrados 142 mm de chuva no mês<br>Mês 2: Foram registrados 138 mm de chuva no mês<br>Mês 3: Foram registrados 100 mm de chuva no mês<br>Mês 4: Foram registrados 80 mm de chuva no mês<br>Mês 5: Foram registrados 180 mm de chuva no mês<br> Mês 6: Foram registrados 157 mm de chuva no mês<br></p>';
     } else if (valor === '30-dias') {
         conteudo.innerHTML = '<p>Precipitação média: 80mm nos últimos 30 dias.</p>';
+    } else {
+        conteudo.innerHTML = '<p>Mês 1: Foram registrados 142 mm de chuva no mês<br>Mês 2: Foram registrados 138 mm de chuva no mês<br>Mês 3: Foram registrados 100 mm de chuva no mês<br>Mês 4: Foram registrados 80 mm de chuva no mês<br>Mês 5: Foram registrados 180 mm de chuva no mês<br> Mês 6: Foram registrados 157 mm de chuva no mês<br></p>';
     }
 });
